33fb8842b75fee90fdf751eecef9a9c3643be4ba
Pedro Falcão <pedro.falcao.ufcg@gmail.com>
2016-08-07 19:03:14 -0300

    minor fixes

--- app/src/main/java/com/example/model/character/CharacterStatus.java
+++ app/src/main/java/com/example/model/character/CharacterStatus.java
@@ -1,6 +1,5 @@
 package com.example.model.character;
 
-import com.example.model.character.status.Status;
 import com.example.model.character.status.energy.Energy;
 import com.example.model.character.status.health.Health;
 
@@ -18,10 +17,10 @@ public class CharacterStatus {
     }
 
     public Health getHealth() {
-        return (Health) health;
+        return health;
     }
 
     public Energy getEnergy() {
-        return (Energy) energy;
+        return energy;
     }
 }
