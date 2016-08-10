8c5772d870bc63469ca3e044da235fe8e42558ce
Pedro Falcão <pedro.falcao.ufcg@gmail.com>
2016-08-10 15:00:11 -0300

    Prototype for a Status Screen and its activity implemented

---
+++ app/src/main/java/com/example/android/app/StatusScreen.java
@@ -0,0 +1,15 @@
+package com.example.android.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+;
+import android.os.Bundle;
+
+public class StatusScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_screen);
+    }
+}
