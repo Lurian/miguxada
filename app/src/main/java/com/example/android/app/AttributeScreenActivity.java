2267c8751f288aaf4cb5aa799e30ce4e0396fdef
Pedro Falcão <pedro.falcao.ufcg@gmail.com>
2016-08-14 00:28:13 -0300

    Implemented AttributeScreenActivity buttons. Aesthetic changes to attribute screen.

--- app/src/main/java/com/example/android/app/AttributeScreenActivity.java
+++ app/src/main/java/com/example/android/app/AttributeScreenActivity.java
@@ -2,10 +2,14 @@ package com.example.android.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.model.character.Character;
 
-public class AttributeScreenActivity extends AppCompatActivity {
+public class AttributeScreenActivity extends AppCompatActivity implements View.OnClickListener {
+    private Character character;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,100 @@ public class AttributeScreenActivity extends AppCompatActivity {
         Character c;
         if (b != null) {
             c = (Character) b.getSerializable("TestChar");
+            this.character = c;
+
+            String agility = c.getAttribute().getAgility().getValue().toString();
+            TextView agiValue = (TextView) findViewById(R.id.agilityValue);
+            agiValue.setText(agility);
+
+            String resilience = c.getAttribute().getResilience().getValue().toString();
+            TextView resiValue = (TextView) findViewById(R.id.resilienceValue);
+            resiValue.setText(resilience);
+
+            String resolve = c.getAttribute().getResolve().getValue().toString();
+            TextView resoValue = (TextView) findViewById(R.id.resolveValue);
+            resoValue.setText(resolve);
+
+            String stamina = c.getAttribute().getStamina().getValue().toString();
+            TextView staValue = (TextView) findViewById(R.id.staminaValue);
+            staValue.setText(stamina);
+
+            String strength = c.getAttribute().getStrength().getValue().toString();
+            TextView strValue = (TextView) findViewById(R.id.strengthValue);
+            strValue.setText(strength);
+
+            String wit = c.getAttribute().getWit().getValue().toString();
+            TextView wValue = (TextView) findViewById(R.id.witValue);
+            wValue.setText(wit);
+
+            final Button agiPlus = (Button) findViewById(R.id.plusButtonAgi);
+            agiPlus.setOnClickListener(this);
+            final Button resiPlus = (Button) findViewById(R.id.plusButtonResi);
+            resiPlus.setOnClickListener(this);
+            final Button resoPlus = (Button) findViewById(R.id.plusButtonReso);
+            resoPlus.setOnClickListener(this);
+            final Button staPlus = (Button) findViewById(R.id.plusButtonSta);
+            staPlus.setOnClickListener(this);
+            final Button strPlus = (Button) findViewById(R.id.plusButtonStr);
+            strPlus.setOnClickListener(this);
+            final Button witPlus = (Button) findViewById(R.id.plusButtonWit);
+            witPlus.setOnClickListener(this);
+            final Button agiMinus = (Button) findViewById(R.id.minusButtonAgi);
+            agiMinus.setOnClickListener(this);
+            final Button resiMinus = (Button) findViewById(R.id.minusButtonResi);
+            resiMinus.setOnClickListener(this);
+            final Button resoMinus = (Button) findViewById(R.id.minusButtonReso);
+            resoMinus.setOnClickListener(this);
+            final Button staMinus = (Button) findViewById(R.id.minusButtonSta);
+            staMinus.setOnClickListener(this);
+            final Button strMinus = (Button) findViewById(R.id.minusButtonStr);
+            strMinus.setOnClickListener(this);
+            final Button witMinus = (Button) findViewById(R.id.minusButtonWit);
+            witMinus.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.plusButtonAgi):
+                character.getAttribute().getAgility().increase(1L);
+                break;
+            case (R.id.plusButtonResi):
+                character.getAttribute().getResilience().increase(1L);
+                break;
+            case (R.id.plusButtonReso):
+                character.getAttribute().getResolve().increase(1L);
+                break;
+            case (R.id.plusButtonSta):
+                character.getAttribute().getStamina().increase(1L);
+                break;
+            case (R.id.plusButtonStr):
+                character.getAttribute().getStrength().increase(1L);
+                break;
+            case (R.id.plusButtonWit):
+                character.getAttribute().getWit().increase(1L);
+                break;
+            case (R.id.minusButtonAgi):
+                character.getAttribute().getAgility().decrease(1L);
+                break;
+            case (R.id.minusButtonResi):
+                character.getAttribute().getResilience().decrease(1L);
+                break;
+            case (R.id.minusButtonReso):
+                character.getAttribute().getResolve().decrease(1L);
+                break;
+            case (R.id.minusButtonSta):
+                character.getAttribute().getStamina().decrease(1L);
+                break;
+            case (R.id.minusButtonStr):
+                character.getAttribute().getStrength().decrease(1L);
+                break;
+            case (R.id.minusButtonWit):
+                character.getAttribute().getWit().decrease(1L);
+                break;
+            default:
+                break;
         }
     }
 }
